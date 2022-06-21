ed2f27963ada105514ad3b8dc57075fd64df64b9
PokeTideBots <103623854+PokeTideBots@users.noreply.github.com>
2022-06-21 15:47:29 +0530

    Update messages.py

--- root/messages.py
+++ root/messages.py
@@ -6,4 +6,5 @@ class Translation(object):
     DOWNLOAD_FAIL_MSG = "**Failed to Download File**❎"
     UPLOAD_MSG = "**Uploading** ⏫"
     UPLOAD_FAIL_MSG = "**Failed to Upload File**❎"
-    UPLOAD_DONE_MSG = "**Uploaded Successfully 💡"
+    UPLOAD_DONE_MSG = "**Uploaded Successfully 💡** \n **✅ Thanks for Using Me...!😌** \n
+**__Made Love❤️with @PokeTide**__"
