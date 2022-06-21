542e55d470ed146ab99d4534d1f9744f250ffc80
PokeTideBots <103623854+PokeTideBots@users.noreply.github.com>
2022-06-21 16:02:36 +0530

    Update messages.py

--- root/messages.py
+++ root/messages.py
@@ -1,7 +1,7 @@
 class Translation(object):
     START_TEXT = "**I'm a Rename and Convert Bot\nJust send me any media to change file name.\nUse /help command for more details **"
 ######################
-    HELP_USER = """**>>Send File/Video\n>>Select desired Option\n>>And Done wait for it to process files**"""
+    HELP_USER = """**>>Send File/Video\n>>Select desired Option\n>>And Done wait for it to process files\n❇️ Powered by 💠 @PokeTide 💠**"""
     DOWNLOAD_MSG = "**Downloading **⏬"
     DOWNLOAD_FAIL_MSG = "**Failed to Download File**❎"
     UPLOAD_MSG = "**Uploading** ⏫"
